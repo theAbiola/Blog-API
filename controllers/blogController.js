2752fd0323d724ae499c4c9270d1b153df847eff
Abdulraheem Habib Abiola <iamhabibabiola@gmail.com>
2025-02-28 22:14:27 +0100

    finish setup for server.js business logic

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,3 +1,44 @@
+// In-memory data store
+let posts = [
+    {
+        id: 1,
+        title: "The Rise of Decentralized Finance",
+        content:
+            "Decentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading.",
+        author: "Alex Thompson",
+        date: "2023-08-01T10:00:00Z",
+    },
+    {
+        id: 2,
+        title: "The Impact of Artificial Intelligence on Modern Businesses",
+        content:
+            "Artificial Intelligence (AI) is no longer a concept of the future. It's very much a part of our present, reshaping industries and enhancing the capabilities of existing systems. From automating routine tasks to offering intelligent insights, AI is proving to be a boon for businesses. With advancements in machine learning and deep learning, businesses can now address previously insurmountable problems and tap into new opportunities.",
+        author: "Mia Williams",
+        date: "2023-08-05T14:30:00Z",
+    },
+    {
+        id: 3,
+        title: "Sustainable Living: Tips for an Eco-Friendly Lifestyle",
+        content:
+            "Sustainability is more than just a buzzword; it's a way of life. As the effects of climate change become more pronounced, there's a growing realization about the need to live sustainably. From reducing waste and conserving energy to supporting eco-friendly products, there are numerous ways we can make our daily lives more environmentally friendly. This post will explore practical tips and habits that can make a significant difference.",
+        author: "Samuel Green",
+        date: "2023-08-10T09:15:00Z",
+    },
+    {
+        id: 4,
+        title: "The importance of consistency",
+        content:
+            "Consistency is what keeps you focused so that you can always do what you say you would do. Consistency is what helps you stay on track and always finish what you started.",
+        author: "Habib Abiola",
+        date: "2025-01-22T17:33:41.660Z",
+    },
+];
+
+let lastId = 3;
+
+const getAllPosts = (req, res) => {
+    res.json(posts);
+}
 
 const getSpecificPost = (req, res) => {
     let postId = parseInt(req.params.id);
@@ -48,4 +89,4 @@ const deletePost = (req, res) => {
     res.sendStatus(200);
 }
 
-export { getSpecificPost, postNewPost, patchPost, deletePost };
+export { getAllPosts, getSpecificPost, postNewPost, patchPost, deletePost };
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,7 +1,27 @@
 import express from 'express';
 import {
+    getAllPosts,
     getSpecificPost,
     postNewPost,
     patchPost,
     deletePost
 } from '../controllers/blogController.js';
+
+const router = express.Router();
+
+//ENDPOINT 1: GET All posts
+router.get("/posts", getAllPosts);
+
+//ENDPOINT 2: GET a specific post by id
+router.get("/posts/:id", getSpecificPost);
+
+//ENDPOINT 3: POST a new post
+router.post("/posts", postNewPost);
+
+//ENDPOINT 4: PATCH a post when you just want to update one parameter
+router.patch("/posts/:id", patchPost);
+
+//ENDPOINT 5: DELETE a specific post by providing the post id.
+router.delete("/posts/:id", deletePost);
+
+export default router;
